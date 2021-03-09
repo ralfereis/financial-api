6a4797a8fcfdaf3b1bf07c25a3b8da4261ad7a1d
Ralfe Reis <ralfe.cavalcante@gmail.com>
2021-03-08 23:06:22 -0300

    Adding middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  request.customer = customer;
+  return next();
+}
+
 app.post('/account', (request, response) => {
   
   const {cpf, name} = request.body;
@@ -29,15 +41,8 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' });
-  }
-  
+app.get('/statement', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
